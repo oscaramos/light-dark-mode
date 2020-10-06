9c423b1d4ea546a0454492036abbd70f797b6b1c
Oscar <oscar.ramos@ucsp.edu.pe>
2020-10-06 13:40:00 -0500

    Change H1 font to Oliver

--- src/components/H1.js
+++ src/components/H1.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 const H1 = styled.h1`
-  font-family: 'Kaushan Script', cursive;
+  font-family: 'Oliver', cursive;
   font-size: 100px;
   margin: 0 0 10px;
 `
